90aed0daac99e34b567fbac17ebc43e1a7ec0c06
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-03-14 07:08:57 +0530

    Time - 0 ms (100 %) , Space - 43.3 MB (78.45 %)

---
+++ 141-LinkedListCycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        //using the fast and slow pointer algorithm
+        ListNode f = head;
+        ListNode s = head;
+       
+        while(f != null && f.next != null){
+            s = s.next;
+            f = f.next.next;
+            
+            if(s == f){
+                return true;
+            }
+        }
+        return false;
+    }
+}
