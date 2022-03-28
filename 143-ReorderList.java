afe316fc07f04af2e3843850c46bb6611e7f38c1
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-03-28 15:36:05 +0530

    Time - 2 ms (68.77 %) , Space - 54.5 MB (30.71 %)

---
+++ 143-ReorderList.java
@@ -0,0 +1,74 @@
+//Question Link - https://leetcode.com/problems/reorder-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middle(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
+    
+     public ListNode reverse(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+    
+    public void reorderList(ListNode head) {
+        //get the middle node
+        //reverse list from middle node
+        //start = head, end = middle node
+        //while (end != null) { end.next = start.next ; start.next = end; }
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = middle(head);
+
+        ListNode hs = reverse(mid);
+        ListNode hf = head;
+
+        // rearrange
+        while (hf != null && hs != null) {
+            ListNode temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+
+        //hf becomes the last node
+        if (hf != null) {
+            hf.next = null;
+        }
+    }
+}
