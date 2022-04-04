5704a6f8324e5dd10590aa5b59de7e11e2dcd3be
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-04-04 07:08:09 +0530

    Time - 0 ms (100 %) , Space - 41.4. MB (76.55 %)

---
+++ 100-SameTree.java
@@ -0,0 +1,34 @@
+//Question Link - https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //base case
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        
+        //recursively validating if the nodes are same or not
+        if(p.val == q.val){
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        }
+        return false;
+    }
+}
