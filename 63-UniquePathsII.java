2953fd081eded2ac77fbaf386b5daaf107eae5e2
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-05-20 06:17:23 +0530

    Time - 0 ms (100 %) , Space - 42 MB (59.25 %)

---
+++ 63-UniquePathsII.java
@@ -0,0 +1,40 @@
+//Question Link - https://leetcode.com/problems/unique-paths-ii/
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int R = obstacleGrid.length;
+        int C = obstacleGrid[0].length;
+
+        //base case
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        // Number of ways of reaching the starting cell = 1.
+        obstacleGrid[0][0] = 1;
+
+        // Filling the values for the first column
+        for (int i = 1; i < R; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1)?1:0;
+        }
+
+        // Filling the values for the first row
+        for (int i = 1; i < C; i++) {
+            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i-1] == 1)?1:0;
+        }
+
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                } 
+                else {
+                    obstacleGrid[i][j] = 0;
+                }
+            }
+        }
+
+        //result is present at the last cell
+        return obstacleGrid[R - 1][C - 1];
+    }
+}
