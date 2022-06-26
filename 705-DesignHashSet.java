993e01a34e54a711f83249821c822109cefec4c5
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-06-26 22:13:48 +0530

    Time - 16 ms (88.21 %) , Space - 56.8 MB (54.93 %)

---
+++ 705-DesignHashSet.java
@@ -0,0 +1,14 @@
+//Question Link - https://leetcode.com/problems/design-hashset/
+
+//built in java class!!!
+class MyHashSet extends HashSet<Integer> {
+    
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
