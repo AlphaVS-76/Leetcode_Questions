0dfec75db63c36ecc3164acda5ccdff804b92026
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-07-07 09:00:45 +0530

    Time - 0 ms (100 %) , Space - 42.8 MB (10.58 %)

---
+++ 709-ToLowerCase.java
@@ -0,0 +1,8 @@
+//Question Link - https://leetcode.com/problems/to-lower-case/
+
+class Solution {
+    public String toLowerCase(String s) {
+        //in built function in java
+        return s.toLowerCase();
+    }
+}
