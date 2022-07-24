e43d66aabbd3ba2402e2e63afc475566a7e543e8
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-07-24 08:10:01 +0530

    Time - 27 ms (5.29 %) , Space - 57.9 MB (45.10 %)

---
+++ 240-Search2DMatrixII.java
@@ -0,0 +1,19 @@
+//Question Link - https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //basic linear search in matrix
+        //we can also implement binary search for more efficiency
+        boolean ans = false;
+        
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[i].length ; j++){
+                if(matrix[i][j] == target){
+                    ans = true;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
