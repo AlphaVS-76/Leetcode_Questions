a3068f6bad554561b9794acf5e1ab6e140f9f344
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-07-28 06:09:22 +0530

    Time - 8 ms (93.26 %) , Space - 47.8 MB (59.97 %)

---
+++ 438-FindAllAnagramsInString.java
@@ -0,0 +1,45 @@
+//Question Link - https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        //minimum window substring
+        List<Integer> ana = new ArrayList();
+        //base case
+        if(s == null || s.length() == 0) {
+            return ana;
+        }
+        
+        int[] temp = new int[26];
+        
+        for(char c: p.toCharArray()) {
+            temp[c - 'a']++;
+        }
+        
+        int start = 0, end, n = s.length(), window = p.length(), count = window;
+        char c;
+        
+        for(end = 0; end < n; end++){
+            if(end-start >= window){
+                c = s.charAt(start);
+                
+                if(++temp[c - 'a'] > 0) {
+                    count++;
+                }
+                
+                start++;
+            }
+            
+            c = s.charAt(end);
+            
+            if(--temp[c-'a'] >= 0) {
+                count--;
+            }
+            
+            if(count == 0) {
+                ana.add(start);
+            }
+        }
+        
+        return ana;
+    }
+}
