c35012d515eab81de96b8e47a3f86baf9c94e3cd
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-07-29 07:15:30 +0530

    Time - 3 ms (54.32 %) , Space - 43.7 MB (10.63 %)

---
+++ 890-Find&ReplacePattern.java
@@ -0,0 +1,42 @@
+//Question Link - https://leetcode.com/problems/find-and-replace-pattern/
+
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> ans = new ArrayList();
+        
+        for (String word: words){
+            if (match(word, pattern)){
+                ans.add(word);
+            }
+        }
+        
+        return ans;
+    }
+    //using hashmaps to keep track of elements with the pattern
+    public boolean match(String word, String pattern) {
+        Map<Character, Character> M = new HashMap();
+        
+        for (int i = 0; i < word.length(); ++i) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+            
+            if (!M.containsKey(w)) {
+                M.put(w, p);
+            }
+            if (M.get(w) != p) {
+                return false;
+            }
+        }
+
+        boolean[] seen = new boolean[26];
+        
+        for (char p: M.values()) {
+            if (seen[p - 'a']) {
+                return false;
+            }
+            seen[p - 'a'] = true;
+        }
+        
+        return true;
+    }
+}
