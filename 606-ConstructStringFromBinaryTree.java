466146e5cab3e5b69830544e0edc4777f860d577
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-09-07 19:46:28 +0530

    Time - 35ms (28.75 %) , Space - 55.1 MB (23.15 %)

---
+++ 606-ConstructStringFromBinaryTree.java
@@ -0,0 +1,34 @@
+//Question Link - https://leetcode.com/problems/construct-string-from-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public String tree2str(TreeNode t) {
+        //base case
+        if(t == null){
+            return "";
+        }
+        if(t.left == null && t.right == null){
+            return t.val + "";
+        }
+        if(t.right == null){
+            return t.val+"("+tree2str(t.left)+")";
+        }
+        
+        //add '(' after a left node and ')' after right node
+        return t.val+"("+tree2str(t.left)+")("+tree2str(t.right)+")";   
+    }
+}
