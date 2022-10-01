e467ba98b4e225cebd27e92bb43475fa80433c69
aishikisikder <91593104+aishikisikder@users.noreply.github.com>
2022-10-01 11:06:00 +0530

    Update 1-TwoSum.java
    just added a simple and efficient solution using python

--- 1-TwoSum.java
+++ 1-TwoSum.java
@@ -16,3 +16,15 @@ class Solution {
         return null;
     }
 }
+
+//Another simple solution of this problem using python
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff = 0
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in nums:
+                idx = nums.index(diff)
+                if i!=idx:
+                    return [i,idx]
