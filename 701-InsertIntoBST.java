ae1cf284799b5d85adcabdb68b6e0328f8f333ff
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-11-26 19:50:18 +0530

    Time - 0 ms (100 %) , Space - 55.8 MB (7.88 %)

---
+++ 701-InsertIntoBST.java
@@ -0,0 +1,34 @@
+//Question Link - https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        //base case
+        if(root == null){
+            return new TreeNode(val);
+        }
+        
+        if(val < root.val){
+            root.left = insertIntoBST(root.left, val);
+        }
+        else{
+            root.right = insertIntoBST(root.right, val);
+        }
+        
+        return root;
+    }
+}
