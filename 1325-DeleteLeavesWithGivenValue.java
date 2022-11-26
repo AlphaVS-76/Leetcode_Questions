294da33abf971dae241740f223ac0eebfb3f960b
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2022-11-26 19:44:59 +0530

    Time - 0 ms (100 %) , Space - 44.6 MB (56.33 %)

---
+++ 1325-DeleteLeavesWithGivenValue.java
@@ -0,0 +1,35 @@
+//Question Link - https://leetcode.com/problems/delete-leaves-with-a-given-value/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        //base case
+        if(root == null){
+            return null;
+        }
+        
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        
+        //remove if its a leaf and ==target
+        if(root.left == null && root.right == null && root.val == target){
+            return null;
+        }
+        
+        return root;
+    }
+}
