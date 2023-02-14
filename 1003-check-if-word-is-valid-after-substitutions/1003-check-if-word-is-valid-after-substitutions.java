7ee4ff281009e9f753c31b31ef2e3ec4b0e338ad
Vaibhav Sharma <76773190+AlphaVS-76@users.noreply.github.com>
2023-02-14 06:29:21 +0530

    Time: 373 ms (6.04%), Space: 42.7 MB (42.95%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -1,9 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        while(s.contains("abc")) {
-            s = s.replaceFirst("abc", "");
+        //base case
+        if(s.length() < 3){
+            return s.isEmpty();
+        }
+        if(s.charAt(s.length()-1)!='c'){
+            return false;
         }
 
-        return s.isEmpty();
+        if(s.contains("abc")) {
+            s = s.replaceFirst("abc", "");
+            return isValid(s);
+        }
+        else{
+            return false;
+        }
     }
 }
